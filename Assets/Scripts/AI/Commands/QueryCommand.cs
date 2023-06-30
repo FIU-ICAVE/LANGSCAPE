c88138106066480cb71b060ea1ec376fbab3b0ac
Jose Gonzalez Lopez <joseg072103@gmail.com>
2023-06-30 14:42:38 -0400

    new commands: query and teleport

---
+++ Assets/Scripts/AI/Commands/QueryCommand.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+class QueryCommand : Command {
+    private GridCellType[] id;
+
+    // Fills the selected region with a specific block.
+    // Syntax Descriptor: <id: int>
+    // q <id1> ...
+    public static new readonly char SIGNATURE = 'q';
+    public static new readonly int REQUIRED_PARAMS = 1;
+
+    public QueryCommand(int[] argv) {
+        id = new GridCellType[argv.Length];
+        for (int i = 0; i < argv.Length; i++) {
+            id[i] = (GridCellType)argv[i];
+        }
+        valid = CODE_VALID;
+    }
+    public override void Execute() {
+        GridMesh.Instance.GetCount(id);
+        //TODO: speak this to the player
+    }
+    public override void Undo() { }
+    public override void Redo() { }
+
+    public override string ToString() {
+        return SIGNATURE +
+        " <" + id + ">";
+    }
+}
---
+++ Assets/Scripts/AI/Commands/TeleportCommand.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class TeleportCommand : Command {
+    private Vector3Int displacement;
+
+    // Fills the selected region with a specific block.
+    // Syntax Descriptor: <displacement: d>
+    // t <dx> <dy> <dz>
+    public static new readonly char SIGNATURE = 't';
+    public static new readonly int REQUIRED_PARAMS = 3;
+
+    public TeleportCommand(int[] argv) {
+        displacement = new Vector3Int(argv[0], argv[1], argv[2]);
+        valid = CODE_VALID;
+    }
+    public override void Execute() {
+        // Set player position
+
+        //updating world stats
+        WorldStateManager.Instance.UpdateTotalWorldObjects();
+    }
+    public override void Undo() {
+        //undo player position
+    }
+    public override void Redo() {
+        //redo player position
+    }
+
+    public override string ToString() {
+        return SIGNATURE +
+        " <" + displacement.x + " " + displacement.y + " " + displacement.z + ">";
+    }
+}
