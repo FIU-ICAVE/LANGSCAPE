197b154d5647f5a2312651af8e0ad2e5edaffa64
CrisBlu <55038118+CrisBlu@users.noreply.github.com>
2023-07-24 13:07:23 -0400

    Fixed UI

--- Assets/CompassSpawning.cs
+++ Assets/CompassSpawning.cs
@@ -7,13 +7,20 @@ public class CompassSpawning : MonoBehaviour
 
     //Gesture Test
     public GestureTest gesture;
+    public GameObject compass;
 
     // Update is called once per frame
     void Update()
     {
-        if(gesture.selected)
-        {
+        if (gesture.selected){
             Debug.Log("Hello");
+
+            compass.SetActive(true);
         }
+        else
+        {
+            compass.SetActive(false);
+        }
+        
     }
 }
