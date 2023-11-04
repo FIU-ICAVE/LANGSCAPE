9756600e6c6626e25079abc809fe4a302ba18e03
Gianni M <gmart280@fiu.edu>
2023-11-03 22:11:17 -0400

    Fixed bugs with background, everything works

--- Assets/Scripts/GroundChanger.cs
+++ Assets/Scripts/GroundChanger.cs
@@ -6,18 +6,19 @@ public class GroundChanger : MonoBehaviour
 {
 
     //public TMPro.TMP_Dropdown GroundDropdown; // for the dropdown menu
-    public GameObject[] GroundStates = new GameObject[6]; // Array of ground state game objects
+    public GameObject[] GroundStates = new GameObject[7]; // Array of ground state game objects
     int area; // Stores Current Active State
 
     void Start() 
     {
         //GroundDropdown.onValueChanged.AddListener(delegate { changeGround(GroundDropdown.value); });
-        GroundStates[1].SetActive(true); //sets the game start area, currently forest
-        area = 1; 
+        GroundStates[0].SetActive(true); //sets the game start area, currently forest
+        area = 1; //keeps track of the land for utility
     }
     
-    public void changeGround(int groundType) 
+    public void changeGround(int groundType2) 
     {
+        int groundType = groundType2 - 1;
         // Deactivates all ground states
         for(int i = 0; i < GroundStates.Length; i++) 
         {
@@ -28,7 +29,7 @@ public class GroundChanger : MonoBehaviour
         if(groundType >= 0 && groundType <= GroundStates.Length) 
         {
             GroundStates[groundType].SetActive(true);
-            area = groundType;
+            area = groundType2;
         }
         else 
         {
