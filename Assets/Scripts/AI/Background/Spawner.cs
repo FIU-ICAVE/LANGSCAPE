3aeb9ee2e53cd299f1d2f920dd62e2c894393874
AgentTailhawk <111831935+AgentTailhawk@users.noreply.github.com>
2023-11-08 13:01:43 -0500

    Update Spawner.cs
    Changed Spawner script to Spawn Objects in front of player

--- Assets/Scripts/AI/Background/Spawner.cs
+++ Assets/Scripts/AI/Background/Spawner.cs
@@ -4,10 +4,33 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // When Called Should Spawn Item
+    [Header("Cameras")]
+    public GameObject occulusCamera;
+    public Camera windowsCamera;
+
+    // When Called Should Spawn Item In front of player
     public GameObject Spawn(GameObject itemPrefab)
     {
-        return (GameObject)Instantiate(itemPrefab, transform.position, Quaternion.identity);
+        Vector3 playerPos;
+        Vector3 playerDir;
+        Quaternion playerRot;
+
+
+#if !UNITY_STANDALONE_WIN
+        playerPos = occulusCamera.transform.position;
+        playerDir = occulusCamera.transform.forward;
+        playerRot = occulusCamera.transform.rotation;
+#else
+        playerPos = windowsCamera.transform.position;
+        playerDir = windowsCamera.transform.forward;
+        playerRot = windowsCamera.transform.rotation;
+#endif
+        // 1 - 5 - 7 - 10 
+        float spawnDist = 7;
+        Vector3 spawnPos = playerPos + playerDir * spawnDist;
+
+
+        return (GameObject)Instantiate(itemPrefab, spawnPos, playerRot);
     }
     public void Despawn(GameObject gameObject)
     {
